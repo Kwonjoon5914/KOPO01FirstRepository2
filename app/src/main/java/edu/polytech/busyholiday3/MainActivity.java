ba2caa175bb87387145931faaf745bb095199698
kjoon5914 <kj4288@naver.com>
2019-06-18 16:39:26 +0900

    add contents to all activities

--- app/src/main/java/edu/polytech/busyholiday3/MainActivity.java
+++ app/src/main/java/edu/polytech/busyholiday3/MainActivity.java
@@ -2,14 +2,24 @@ package edu.polytech.busyholiday3;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class MainActivity extends AppCompatActivity {
     EditText idEdit, passEdit;
@@ -18,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView MAImageView1 = (ImageView)findViewById(R.id.MAImageView1);
+        Glide.with(this).load(R.drawable.rabbit).into(MAImageView1);
 
         idEdit = (EditText) findViewById(R.id.CTWEditText1);
         passEdit = (EditText) findViewById(R.id.CTWEditText2);
--- app/src/main/java/edu/polytech/busyholiday3/Signup.java
+++ app/src/main/java/edu/polytech/busyholiday3/Signup.java
@@ -2,7 +2,11 @@ package edu.polytech.busyholiday3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Signup extends AppCompatActivity {
 
@@ -11,6 +15,21 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        Button SUButton1 = (Button) findViewById(R.id.SUButton1);
+        SUButton1.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        members.class);
+                startActivity(intent);
+
+                final String resultText;
+
+                Toast.makeText(Signup.this,"로그인 되었습니다" ,Toast.LENGTH_LONG).show();
+                    startActivityForResult(intent, 1);
+
+            }
+        });
     }
 }
