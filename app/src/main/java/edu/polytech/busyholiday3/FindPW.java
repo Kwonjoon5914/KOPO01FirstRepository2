d229a0601987c2ef70206d5860ffb00bc43fd945
kjoon5914 <kj4288@naver.com>
2019-06-18 13:59:42 +0900

    add FindPW.java, activity_find_pw.xml

---
+++ app/src/main/java/edu/polytech/busyholiday3/FindPW.java
@@ -0,0 +1,14 @@
+package edu.polytech.busyholiday3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindPW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pw);
+    }
+}
